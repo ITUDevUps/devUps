db48c586e30c71a5f79f908f32180049f3eb406d
MaFi050599 <89101190+MaFi050599@users.noreply.github.com>
2023-02-08 11:54:59 +0100

    Added ef scraffolding from original DB

--- minitwit-backend/Data/Message.cs
+++ minitwit-backend/Data/Message.cs
@@ -1,6 +1,17 @@
-﻿namespace minitwit_backend.Data
+﻿using System;
+using System.Collections.Generic;
+
+namespace minitwit_backend.Data;
+
+public partial class Message
 {
-    public class Message
-    {
-    }
+    public long MessageId { get; set; }
+
+    public long AuthorId { get; set; }
+
+    public byte[] Text { get; set; } = null!;
+
+    public long? PubDate { get; set; }
+
+    public long? Flagged { get; set; }
 }
