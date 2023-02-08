e15d2245a9859c2d9961693720c44c825a963e94
MaFi050599 <89101190+MaFi050599@users.noreply.github.com>
2023-02-08 11:17:36 +0100

    initial setup for aspnetcore simple api

--- minitwit-backend/Program.cs
+++ minitwit-backend/Program.cs
@@ -1,13 +1,9 @@
-using System.Reflection;
-using minitwit_backend.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddEntityFrameworkSqlite().AddDbContext<MinitwitContext>();
 
 var app = builder.Build();
 
@@ -18,14 +14,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
-
-
 app.UseHttpsRedirection();
 
 
-app.MapGet("/GetMessages",async () => await MessageRepository.GetMessagesAsync());
-app.MapGet("/GetMessages/{userName}", async (string userName) => await MessageRepository.GetMessagesAsyncByUserName(userName));
+
 
 app.Run();
 
