b0cd68d90c417421be4e168843ba1dd7382a2596
MaFi050599 <89101190+MaFi050599@users.noreply.github.com>
2023-02-08 14:31:59 +0100

    Added db context and DB entities

---
+++ minitwit-backend/Data/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace minitwit_backend.Data;
+
+public partial class User
+{
+    public long UserId { get; set; }
+
+    public byte[] Username { get; set; } = null!;
+
+    public byte[] Email { get; set; } = null!;
+
+    public byte[] PwHash { get; set; } = null!;
+}
