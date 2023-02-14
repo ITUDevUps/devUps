e09ab855fcf87e8d56ca6ed40e76b358c722ef8e
Theodor <theodorkier@protonmail.com>
2023-02-14 13:52:57 +0100

    implement simple hacky routing

--- minitwit-frontend/src/App.tsx
+++ minitwit-frontend/src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
+
+    const showHome = window.location.pathname === "/";
+    const showLogin = window.location.pathname === "/login";
+    const showRegister = window.location.pathname === "/sign-up";
+
     return (
         <div className="App">
             <header className="App-header">
-                <Home/>
+                <h1>MiniTwit</h1>
+                <div className="navigation">
+                    <a className="nav-link" href="/">Public Timeline</a> |
+                    <a className="nav-link" href="/login">Login</a> |
+                    <a className="nav-link" href="/sign-up">Sign up</a>
+                </div>
+                {showHome ? <Home/> : undefined}
+                {showLogin ? <Login/> : undefined}
+                {showRegister ? <Register/> : undefined}
+
             </header>
         </div>
     );
--- minitwit-frontend/src/pages/Home/Home.tsx
+++ minitwit-frontend/src/pages/Home/Home.tsx
@@ -1,8 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import Message, {IMessage} from "../../components/message/Message";
 import "./Home.css";
-import Login from "../Login/Login";
-import Register from "../Register/Register";
+
 function Home() {
 
     const [messages, setMessages] = useState([]);
@@ -16,35 +15,23 @@ function Home() {
             })
     }, [])
 
-        // @ts-ignore
+    // @ts-ignore
     return (
-            <div className="home-container">
-                <div className="header">
-                    <h1>MiniTwit</h1>
-                </div>
-                <div className="navigation">
-                    <a className="nav-link" href="/">Public Timeline</a> |
-                    <a className="nav-link" href="/login">Login</a> |
-                    <a className="nav-link" href="/sign_up">Sign up</a>
-                </div>
-                < Login/>
-                < Register/>
-                <div className="timeline">
-                    {messages ? messages.map((message: IMessage, index) => (
-                        <Message
-                            key={index}
-                            image={"http://uploads.neatorama.com/images/posts/376/63/63376/1373756607-0.jpg"}
-                            userName={message.userName}
-                            message={message.message}
-                            date={new Date(parseInt(message.date) * 1000).toLocaleString()}
-                        />
-                    )) : undefined}
-                </div>
-
+        <div className="home-container">
+            <div className="timeline">
+                {messages ? messages.map((message: IMessage, index) => (
+                    <Message
+                        key={index}
+                        image={"http://uploads.neatorama.com/images/posts/376/63/63376/1373756607-0.jpg"}
+                        userName={message.userName}
+                        message={message.message}
+                        date={new Date(parseInt(message.date) * 1000).toLocaleString()}
+                    />
+                )) : undefined}
             </div>
-        );
+        </div>
+    );
 }
-
 
 
 export default Home;
