6af784b1dcb4e4b4c25a3b326979ab2372887cbc
Theodor <theodorkier@protonmail.com>
2023-02-14 18:15:54 +0100

    states for login and register

--- minitwit-frontend/src/pages/Login/Login.tsx
+++ minitwit-frontend/src/pages/Login/Login.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Login.css';
 
 function Login() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <div>
             <h1>Login</h1>
@@ -9,11 +13,11 @@ function Login() {
                 <label htmlFor="username">
                     Username:
                 </label>
-                <input id="username" className="login-input" type="text" name="username"/>
+                <input id="username" className="login-input" type="text" name="username" autoFocus value={username} onChange={e => setUsername(e.target.value)}/>
                 <label htmlFor="password">
                     Password:
                 </label>
-                <input id="password" className="login-input" type="password" name="password"/>
+                <input id="password" className="login-input" type="password" name="password" value={password} onChange={e => setPassword(e.target.value)}/>
                 <input className="login-submit" type="submit" value="Login"/>
             </form>
         </div>
--- minitwit-frontend/src/pages/Register/Register.tsx
+++ minitwit-frontend/src/pages/Register/Register.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Register.css';
 
 function Register() {
+
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <div>
             <h1>Sign Up</h1>
@@ -9,19 +14,19 @@ function Register() {
                 <label htmlFor="username">
                     Username:
                 </label>
-                <input id="username" className="register-input" type="text" name="username"/>
+                <input id="username" className="register-input" type="text" name="username" value={username} onChange={e => setUsername(e.target.value)} autoFocus required/>
                 <label htmlFor="email">
                     E-mail:
                 </label>
-                <input id="email" className="register-input" type="email" name="email"/>
+                <input id="email" className="register-input" type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} required/>
                 <label htmlFor="password">
                     Password:
                 </label>
-                <input id="password" className="register-input" type="password" name="password"/>
+                <input id="password" className="register-input" type="password" name="password" minLength={8} value={password} onChange={e => setPassword(e.target.value)} required/>
                 <label htmlFor="repeatPassword">
                     Password (repeat):
                 </label>
-                <input id="repeatPassword" className="register-submit" type="password" name="password"/>
+                <input id="repeatPassword" className="register-submit" type="password" name="password" minLength={8} required/>
                 <input className="register-submit" type="submit" value="Sign Up"/>
             </form>
         </div>
