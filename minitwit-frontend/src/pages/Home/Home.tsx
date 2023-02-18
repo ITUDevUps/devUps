e7a3cb41b970c90d02fb2fbc7da7ba80a6a0702b
Theodor <theodorkier@protonmail.com>
2023-02-18 17:05:49 +0100

    implement input area for twits

--- minitwit-frontend/src/pages/Home/Home.tsx
+++ minitwit-frontend/src/pages/Home/Home.tsx
@@ -21,11 +21,18 @@ function Home() {
     // @ts-ignore
     return (
         <div className="home-container">
+            <div>
+                <form className="post-message-form">
+                    <textarea id="message" className="post-message-input" name="message" placeholder="What's on your mind?" required/>
+                    <input className="post-message-submit" type="submit" value="Post"/>
+                </form>
+
+            </div>
             <div className="timeline">
                 {messages.length !== 0 ? messages.map((message: IMessage, index) => (
                         <Message
                             key={index}
-                            image={"http://uploads.neatorama.com/images/posts/376/63/63376/1373756607-0.jpg"}
+                            image={"https://uploads.neatorama.com/images/posts/376/63/63376/1373756607-0.jpg"}
                             userName={message.userName}
                             message={message.message}
                             date={new Date(parseInt(message.date) * 1000).toLocaleString()}
