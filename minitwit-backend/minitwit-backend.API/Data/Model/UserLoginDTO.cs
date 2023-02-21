762380e4f28eec68ded3ebcb0f67d01aea51520d
MaFi050599 <89101190+MaFi050599@users.noreply.github.com>
2023-02-21 16:37:12 +0100

    added login and register with pw hash more files

---
+++ minitwit-backend/minitwit-backend.API/Data/Model/UserLoginDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace minitwit_backend.Data.Model
+{
+    public class UserLoginDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
