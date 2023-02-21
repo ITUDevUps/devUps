a6da526c14180c6ab57e317b36faca8e4b612510
MaFi050599 <89101190+MaFi050599@users.noreply.github.com>
2023-02-21 16:35:59 +0100

    added login and register with pw hash

--- minitwit-backend/minitwit-backend.API/Data/IUserRepository.cs
+++ minitwit-backend/minitwit-backend.API/Data/IUserRepository.cs
@@ -4,6 +4,9 @@ namespace minitwit_backend.Data;
 
 public interface IUserRepository : IDisposable
 {
+    public Task RegisterUser(RegisterUserDTO user);
     public Task RegisterUser(ApiSimUser apiSimUser);
+
+    public Task<int> VerifyLogin(UserLoginDTO userLoginDTO);
     public bool TryGetUserId(string username, out int id);
 }
