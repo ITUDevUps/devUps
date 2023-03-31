35dd2bb5d739f0e67d41e2961a80ba5904c07b52
Theodor <theodorkier@protonmail.com>
2023-03-31 16:09:08 +0200

    prepare userpage for context

--- minitwit-frontend/src/pages/User/UserPage.tsx
+++ minitwit-frontend/src/pages/User/UserPage.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Timeline from "../../components/Timeline/Timeline";
 import {useParams} from "react-router-dom";
 import "./UserPage.css";
 import FollowUserButton from "../../components/FollowUserButton/FollowUserButton";
+import {UserContext} from "../../Util/Contexts/UserContext";
 
 function UserPage() {
 
+	const {user} = useContext(UserContext)
 	let {userName} = useParams();
 	let userNameCapitalized: string = "Incognito";
 	if (userName !== undefined) {
@@ -15,7 +17,9 @@ function UserPage() {
 	return (
 		<div className="container">
 			<h2 className="user-title">{userNameCapitalized}</h2>
-			<FollowUserButton userName={userName as string}/>
+			{userName !== user?.userName ? (
+				<FollowUserButton userName={userName as string}/>
+			) : null}
 			<Timeline endpoint={`/GetMessages/${userName}`}/>
 		</div>
 	);
