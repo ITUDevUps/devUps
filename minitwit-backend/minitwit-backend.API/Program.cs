563353d0e11238d4ec7368ae76678f0c2d902e95
Theodor <theodorkier@protonmail.com>
2023-04-18 14:57:39 +0200

    prometheus grafana edits

--- minitwit-backend/minitwit-backend.API/Program.cs
+++ minitwit-backend/minitwit-backend.API/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using minitwit_backend.Controllers;
 using minitwit_backend.Data;
+using Prometheus;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +17,9 @@ builder.Services.AddCors();
 
 
 var app = builder.Build();
+
+app.UseMetricServer(3005);
+app.UseHttpMetrics();
 
 app.RegisterMapMiddleware();
 
